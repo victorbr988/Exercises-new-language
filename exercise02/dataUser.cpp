cd8fee9b949e0086e4abf0be0dbc09984aa58090
Victor Gabriel <gabrielvghs666@gmail.com>
2022-11-13 16:08:29 -0300

    feat: save data profile user

---
+++ exercise02/dataUser.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main(int argc, char** argv){
+  char nome[30];
+  char endereco[30];
+  int idade;
+
+  printf("Nome \n");
+  scanf("%s", &nome);
+
+  printf("Endereco \n");
+  scanf("%s", &endereco);
+
+  printf("Idade \n");
+  scanf("%d", &idade);
+
+  printf("Nome: %s \n", nome);
+  printf("Endereco: %s \n", endereco);
+  printf("Idade: %d", idade);
+
+  return 0;
+}
