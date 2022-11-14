53ea16d6d7352114465b8aa1618fd3a407a0f714
Victor Gabriel <gabrielvghs666@gmail.com>
2022-11-14 15:04:10 -0300

    feat: add conditional message

--- exercise03/media.cpp
+++ exercise03/media.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-int main(int argc) {
+int main(int argc, char ** argv) {
   float nota1;
   float nota2;
   float nota3;
@@ -20,6 +20,12 @@ int main(int argc) {
   scanf("%f", &nota4);
 
   media = (nota1 + nota2 + nota3 + nota4) / 4;
-
+  
+  if(media > 7) {
+    printf("Você está aprovado \n");
+  }
+  else {
+    printf("Você está reprovado \n");
+  }
   printf("A sua média é %f", media);
 }
